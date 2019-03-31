24b53175e5382e5d91dfbfc350f5ba30f2595227
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:00:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+    if @@cart.length == 0 
+      ""
     @@cart.each do |item|
       resp.write "#{item}\n"
     end   
