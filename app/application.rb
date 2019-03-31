9cc0f2fd7bccdfc10947ce0bcf125ddc55462861
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:07:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,9 +28,9 @@ class Application
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
       else 
-        
+        resp.write "We don't have that item"
       end   
-  end
+    end
     else
       resp.write "Path Not Found"
     end
