369ab1d18d7df02c6d9c1e58d85a689f12821186
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:01:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,10 +15,12 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-    if @@cart.length == 0 
-      ""
-    @@cart.each do |item|
+      if @@cart.length == 0 
+        ""Your cart is empty""
+      else
+      @@cart.each do |item|
       resp.write "#{item}\n"
+      end
     end   
     else
       resp.write "Path Not Found"
